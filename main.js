f45600cf2e0972e6b0d674df82dc37c86bad9519
saachikubadia <93033395+saachikubadia@users.noreply.github.com>
2022-10-09 16:48:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 https://teachablemachine.withgoogle.com/models/Py0RVhnBE/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='"+data_uri+"'/>";   
+     });
+}
+
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Py0RVhnBE/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('model is loaded');
+}
