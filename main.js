79fb46fc08bf201919cd89849e6df6ef15a6755d
saachikubadia <93033395+saachikubadia@users.noreply.github.com>
2022-10-17 15:26:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,18 @@ classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/
 function modelLoaded(){
     console.log('model is loaded');
 }
+
+function check(){
+    img=document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
